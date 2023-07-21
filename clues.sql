05a98c5ec746dc1b37acc2aba67394bdbaa23a79
Sandy <sandyahmedy7@gmail.com>
2023-07-21 13:49:01 -0500

    lab complete

--- clues.sql
+++ clues.sql
@@ -71,5 +71,4 @@ WHERE CO.code = 'BRA';
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
 
-SELECT * FROM city
-WHERE population = 91084
+SELECT * FROM cityWHERE population = 91084
